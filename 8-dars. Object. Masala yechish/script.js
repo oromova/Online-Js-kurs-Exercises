8d9c61b42f8102141c5687bc7052a5bd1423f088
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-05-10 01:47:45 +0300

    homeworkdan 1ta masala ishlandi

--- 8-dars. Object. Masala yechish/script.js
+++ 8-dars. Object. Masala yechish/script.js
@@ -1,5 +1,68 @@
 // ======== 8-dars Object. Masalalar. =========
 
+// obj = {
+//   name: 'WebBrain',
+//   surname: 'acAdemy',
+//   age: 123,
+//   status: false
+// }
+
+// // res = 'WBA'
+// res = {}
+// for ( key in obj) {
+//   if( typeof obj[key] === 'string'){
+//     if(obj[key] == /[A-Z]/i ){
+//     console.log(res = {...res, [k]: obj[k]})
+//     }
+//   }
+// }
+
+
+// {string: 3, number: 1, boolean: 1}
+obj = {
+  name: 'webbrain',
+  surname: 'academy',
+  age: 123,
+  status: false,
+  title: 'it'
+}
+
+function count () {
+  let str = 0;
+  let num = 0;
+  let boolean = 0;
+
+  for (k in obj){
+  if (typeof obj[k] === 'string') {
+     str++;
+  } else if (typeof obj[k] === 'number'){
+     num++;
+  } else {
+     boolean++;
+  }
+}
+console.log(`string ${str}ta`);
+console.log(`number ${num}ta`);
+console.log(`boolean ${boolean}ta`);
+}
+count()
+
+
+
+//Homework kanalidan 8-savol
+let user = {
+  name: 'webbrain',
+  surname: "academy",
+  getName() {
+    console.log(user.name, user.surname);
+  },
+};
+
+user.getName()
+
+
+// W3RECOURSE.COM
+
 // 1. Write a JavaScript program to list the properties of a JavaScript object.
 // Sample Output: name,sclass,rollno
 
@@ -44,3 +107,4 @@
 // }
 
 // console.log(Silindr( 8, 4))
+
