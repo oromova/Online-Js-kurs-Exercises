cda0a439ad79007102e6805e0362bd4ede2edce3
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-05-21 13:57:03 +0300

    Diogonal bo'yicha array ichidagi ma'lumotlarni chiqaring

---
+++ 11-dars. Array-1/script.js
@@ -0,0 +1,12 @@
+// diogonal bo'yicha array ichidagi raqamlarni chiqaring
+// res = 1, 5, 9
+
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (i in arr){
+  console.log(arr[i][i]);
+}
