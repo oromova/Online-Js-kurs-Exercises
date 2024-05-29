1bbe7354ca890a489d508293dcf3768ed621fc5a
Yulduz_Oromova <mahmurbekovna@gmail.com>
2024-05-30 00:54:23 +0300

    Masalalardagi xatoliklar to'g'irlandi

--- 13-dars. Array-3 CRUD/script.js
+++ 13-dars. Array-3 CRUD/script.js
@@ -14,6 +14,12 @@ const a = (str) => {
 
 a("Webbr@in Ac@demy"); // Webbrain Academy
 
+// 2-yo'l.
+str = str.split("");
+str = str.map((val) => (val === "@" ? "a" : val));
+str = str.join("");
+console.log(str); // Webbrain Academy
+
 // 2. Duplicate bo'lgan harflarni o'chiring
 
 let st = "webbbraiiiin accaddemmy";
@@ -27,7 +33,15 @@ const duplicate = (str) => {
   console.log(res);
 };
 
-duplicate(st); // webrain academy
+//duplicate(st); // webrain academy
+
+// Hamma duplicatlarni o'chiring hamma arfdan 1tadan bo'lsin.
+
+let res = "";
+for (let i of st) {
+  if (!res.includes(i)) res += i;
+}
+console.log(res); // webrain cdmy
 
 // 3. String ichidagi raqamlarni toping.
 
@@ -62,68 +76,116 @@ dubleLetter("Webbrain"); // WWeebbbbrraaiinn
 // 5. Berilgan so'z palindrom yoki palindrom emasligini tekshiring
 // non, kichik, kiyik, kuchuk
 
-const checkPalindrom = (str) => {
-  let strRev = '' 
-  if (str.includes("sh") && str.includes("ch") && str.includes("ng")) {
-   strRev += strRev = str.split('').join('')
-   console.log('Bu soz palindrom');
-  } else if (str == strRev) {
-   strRev += str.split("").reverse().join("");
-    console.log('Bu soz palindrom');
-  }
-  else console.log("Bu so'z palindrom emas");
-  console.log(strRev);
-};
+// const checkPalindrom = (str) => {
+//   let strRev = "";
+//   if (str.includes("sh") && str.includes("ch") && str.includes("ng")) {
+//     strRev += strRev = str.split("").join("");
+//     console.log("Bu soz palindrom");
+//   } else if (str == strRev) {
+//     strRev += str.split("").reverse().join("");
+//     console.log("Bu soz palindrom");
+//   } else console.log("Bu so'z palindrom emas");
+//   console.log(strRev);
+// };
 
-checkPalindrom("kichik");
+// checkPalindrom("kichik");
 
+// BU MASALANING OPTIMAL TEZ ISHLAYDIGAN YO'LI
+
+let s = "kiyik";
+let ress = "so'z palindrom";
+for (let i = 0; i < str.length / 2; i++) {
+  if (str[i] !== str.at(-i - 1)) res = "soz palindrom emas";
+  break;
+}
+console.log(res);
 
 // 6. Berilgan stringlardagi harflar soni bir-biriga teng
 // yoki teng emasligini aniqlang. tartibi muhim emas
 
-let str1 = 'web'
-let str2 = 'ewb'
+// 1-yo'l
+let a1 = "web";
+let b1 = "ewb";
 
-console.log(str1.localeCompare('str2'));
+console.log(a1.split("").sort().join(""));
+console.log(b1.split("").sort().join(""));
+// taqqoslasa teng chiqadi
+
+// 2-yo'l
+function check(a, b) {
+  if (a1.length === b1.length) {
+    for (let str of a1) {
+      let reg = new RegExp(str, "g");
+
+      if (a1.match(reg).length === b1.match(reg).length) {
+        continue;
+      } else return false;
+    }
+  } else return false;
+  return true;
+}
+
+//console.log(check(a1, b1));
+
+// 3-yo'l
+let a2 = {};
+let b2 = {};
+
+function check(a, b) {
+  if (a1.length === b1.length) {
+    for (let i in a1) {
+      a2[a1[i]] = (a2[a1[i]] || 0) + 1;
+      b2[b1[i]] = (b2[b1[i]] || 0) + 1;
+    }
+    for (let i of a1) {
+      if (a2[i] !== b2[i]) return false;
+    }
+  } else return false;
+  console.log(a2);
+  console.log(b2);
+  return true;
+}
+console.log(check(a1, b1));
 
 // 7. Stringni ziplash
 
-let zip = "webbraaiiin"
+let zip = "webbraaiiin";
+let result = "";
 
-const letZip = (str) => {
-for (i in str) {
-  console.log(str[i] === str[i]);
-  if (str[i] === str[i - i]){
-    console.log();
-  }
-}
-}
+const letZip = (zip) => {
+  let count = 1;
+  for (let i in zip) {
+    if (zip[+i + 1] === zip[i]) {
+      count++;
+    } else {
+      result += `${zip[i]}${count > 1 ? count : ""}`;
+      count = 1
+    }
+  }console.log(result)
+};
 
-letZip(zip)
+letZip(zip);     // web2ra2i3n
 
-
-
-// array 
+// array
 // 2. arraylarni bir biriga taqqoslang
 
 const compareArr = (ar, ar2) => {
-  if(ar.toString() === ar2.toString()) {
-    console.log('Arraylar bir biriga teng');
-  }else {
-    console.log('Arraylar bir biriga teng emas');
+  if (ar.toString() === ar2.toString()) {
+    console.log("Arraylar bir biriga teng");
+  } else {
+    console.log("Arraylar bir biriga teng emas");
   }
-}
-compareArr([1, 2, 3], [1, 2, 3])
-
+};
+compareArr([1, 2, 3], [1, 2, 3]);
 
 // 3 Arraydagi ikki xonali sonlarni chiqaring
-let arr = [1, 64, 8, 27, 64, 125]
+let arr = [1, 64, 8, 27, 64, 125];
 
 const checkDigitsNum = (ar) => {
   for (i of ar) {
-    if(i > 9 && i < 99){
+    if (i > 9 && i < 99) {
       console.log(`${i} ikki xonali son`);
-    }else console.log(`${i} ikki xonali son emas`);
+    } else console.log(`${i} ikki xonali son emas`);
   }
-}
-checkDigitsNum(arr)
+};
+checkDigitsNum(arr);
